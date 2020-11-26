446672652abbd29878af04ce98fec8966661a190
Maciej <maciejkoc0@gmail.com>
2020-11-25 18:11:47 -0600

    Initial Commit

---
+++ Understanding_C_Pointers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Understanding_C_Pointers
+//
+//  Created by Maciej on 11/25/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
